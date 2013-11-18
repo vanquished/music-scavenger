9e15516b2cfa241e3bae4506466dcb6cc3a64319
vanquished <predictionsofyesterday@gmail.com>
2013-11-17 18:04:18 -0700

    added a little more functionality

--- main.py
+++ main.py
@@ -3,6 +3,26 @@
 
 #uses
 import urllib
+import sys
 #init vars
-
+line = 0
+songline = []
+song = sys.argv[1]
+artist = sys.argv[2]
+query = "http://www.ultimate-guitar.com/search.php?title="+song+" "+artist+"&type%5B2%5D=300&page=1&tab_type_group=text&app_name=ugt&order=myweight&rating%5B0%5D=4&rating%5B1%5D=5"
 #Begin
+print query
+open_url = urllib.urlopen(query)
+data = open_url.read()
+f = open("urlfile", "w+r")
+f.seek(0)
+f.truncate()
+f.write(data)
+
+#not working
+for line in f:
+    if 'song' in line:
+        songline.append(line)
+print line
+f.close()
+print songline
